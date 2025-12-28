ebf299d3775141d56ef818a36ec807b6f1be2453
Jeromandres <44743048+Jeromandres@users.noreply.github.com>
2025-12-28 09:43:25 +0100

    Update OpenEisenhowerViewCommand.ts

--- src/Commands/OpenEisenhowerViewCommand.ts
+++ src/Commands/OpenEisenhowerViewCommand.ts
@@ -1,1 +1,15 @@
+import { App, Command } from "obsidian";
+import { EisenhowerView } from "../Views/EisenhowerView";
 
+export class OpenEisenhowerViewCommand implements Command {
+  id = "open-eisenhower-view";
+  name = "Open Eisenhower View";
+
+  constructor(private app: App) {}
+
+  callback = async () => {
+    const leaf = this.app.workspace.getLeaf("tab");
+    await leaf.setViewState({ type: EisenhowerView.viewType, active: true });
+    this.app.workspace.revealLeaf(leaf);
+  };
+}
